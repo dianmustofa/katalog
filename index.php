0570531f3b632ba4e45c6a18578553f1654e8690
dianmustofa <54433667+dianmustofa@users.noreply.github.com>
2024-06-08 14:18:21 +0700

    update BASE_URL

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php define('BASE_URL', 'http://'.$_SERVER['HTTP_HOST'].'/apimobile/katalog'); ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -7,24 +8,24 @@
     
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/animate.css">
     
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/magnific-popup.css">
 
-    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/aos.css">
 
-    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/ionicons.min.css">
 
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/jquery.timepicker.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/jquery.timepicker.css">
 
     
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/flaticon.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/icomoon.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/style.css">
   </head>
   <body>
     
@@ -166,21 +167,21 @@
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/jquery.waypoints.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.animateNumber.min.js"></script>
-  <script src="js/scrollax.min.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/jquery.min.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/popper.min.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/bootstrap.min.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/jquery.easing.1.3.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/jquery.waypoints.min.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/jquery.stellar.min.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/owl.carousel.min.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/jquery.magnific-popup.min.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/aos.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/jquery.animateNumber.min.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/scrollax.min.js"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="js/google-map.js"></script>
-  <script src="js/main.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/google-map.js"></script>
+  <script src="<?php echo BASE_URL; ?>/js/main.js"></script>
     
   </body>
 </html>
